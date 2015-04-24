7ea3b9b287b51eff71c31b7d19edfb1cec1223ed
Rodrigo Navarro <rnavarro1@gmail.com>
2015-04-24 19:04:05 -0300

    Ajustando o servidor de arquivos estático.

--- lib/mini_node/file.rb
+++ lib/mini_node/file.rb
@@ -1,9 +1,19 @@
+require "mime/types"
+
 require "mini_node/stream"
 
 module MiniNode
   class File < Stream
     def size
       @fd.size
+    end
+
+    def name
+      ::File.basename(@fd.path)
+    end
+
+    def content_type
+      MIME::Types.of(name).first
     end
   end
 end
