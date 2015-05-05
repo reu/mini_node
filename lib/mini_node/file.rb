426298b59eef5b97d3ff27ac518bb962aedbfa1e
Rodrigo Navarro <rnavarro1@gmail.com>
2015-05-05 20:59:41 -0300

    Não começar a ler o arquivo automaticamente quando ele for aberto.

--- lib/mini_node/file.rb
+++ lib/mini_node/file.rb
@@ -4,8 +4,26 @@ require "mini_node/stream"
 
 module MiniNode
   class File < Stream
+    def initialize(*args)
+      super
+      @reading = false
+    end
+
+    def read
+      @reading = true
+    end
+
     def size
       @fd.size
+    end
+
+    def handle_read
+      super if @reading
+    end
+
+    def pipe(*args)
+      @reading = true
+      super
     end
 
     def name
